ee691bf755aaa6018b6a60ddb62d1d13a1f881c9
blakehulett7 <blake.hulett7@gmail.com>
2024-05-06 20:38:16 -0500

    Fourth task completed. We now sort the occurences of each letter.

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def main():
     book_path = "books/frankenstein.txt"
     contents = read_book(book_path)
     count = word_count(contents)
-    print(letter_count(contents))
+    letters = letter_count(contents)
+    sorted_letters = sort_letters(letters)
+    print(f"--- Begin Report of {book_path} ---")
+    print(f"{count} words found in the document")
+    print(sorted_letters)
+    
     
 def read_book(book_path):
     with open(book_path) as f:
@@ -20,5 +25,10 @@ def letter_count(contents):
     for letter in letter_list:
         count_dict[letter] = lowercase_contents.count(letter)
     return count_dict
+
+def sort_letters(letters):
+    items = letters.items()
+    sorted_items = sorted(items, reverse = True, key = lambda item: item[1])
+    return dict(sorted_items)
     
 main()
